a04e4875e54f9da25f8f3df42a2894a6768e3d7f
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-01-17 00:21:56 +0530

    adding exclusive offers features

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FeatureDestination from '../components/FeatureDestination'
 import Hero from '../components/Hero'
+import ExclusiveOffers from '../components/ExclusiveOffers'
 Hero
 const Home = () => {
   return (
     <>
         <Hero/>
         <FeatureDestination/>
+        <ExclusiveOffers/>
     </>
   )
 }
