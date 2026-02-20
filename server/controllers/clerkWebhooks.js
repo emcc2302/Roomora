e79d44160596c966869b989c37d853f9fc0badd2
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-02-21 00:25:12 +0530

    Create Vercel.json

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,48 @@
+import User from '../models/User.js';
+import { Webhook } from 'svix';
+
+const clerkWebhookSecret =async (req, res) => {
+    try{
+        //create a new instance of the Webhook class with the secret key
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+        //Getting Headers
+        const headers = {
+            'svix-id': req.headers['svix-id'],
+            'svix-timestamp': req.headers['svix-timestamp'],
+            'svix-signature': req.headers['svix-signature']
+        };
+        await whook.verify(JSON.stringify(req.body), headers);
+
+        //Getting Data from the request body
+        const { data, type } = req.body;
+        const userData={
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + " " + data.last_name,
+            image:data.image_url,
+        }
+
+        //Switch case to handle different types of events
+        switch (type) {
+            case 'user.created':
+                await User.create(userData);
+                break;
+            case 'user.updated':
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            case 'user.deleted':
+                await User.findByIdAndDelete(data.id,);
+                break;
+
+            default:
+                break;
+        }
+        res.status(200).json({ message: "Webhook received successfully" });
+    } catch (error) {
+        console.error("Error processing webhook:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+
+export default clerkWebhookSecret;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,21 @@ import "dotenv/config";
 import cors from 'cors';
 import { createServer } from 'http';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import clerkWebhooks from './controllers/clerkWebhooks.js';
 // import { Server } from 'socket.io';
 // import { router } from './routes.js';
 
 connectDB();
 const app = express();
 app.use(cors());// Enable CORS for all routes
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+//API to listen to the webhook events from Clerk
+app.use("/api/clerk",clerkWebhooks)
+
 app.get('/', (req, res) => {
     res.send('Api is running fine');
 });
