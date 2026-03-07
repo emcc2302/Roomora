38c5418085826e7d83568bd778510c9a66d7337d
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-03-08 00:59:19 +0530

    removed multer (not needed)

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,98 +1,212 @@
+// import Room from "../models/Room.js";
+// import Hotel from "../models/Hotel.js";
+// import { v2 as cloudinary } from "cloudinary";
+
+// // create room
+// export const createRoom = async (req, res) => {
+//   try {
+
+//     const userId = req.user._id;
+
+//     const { roomType, pricePerNight, amenities } = req.body;
+
+//     const hotel = await Hotel.findOne({ owner: userId });
+
+//     if (!hotel) {
+//       return res.json({ success:false, message:"Hotel not found" });
+//     }
+
+//     const uploadedImages = req.files.map(async (file)=>{
+//       const response = await cloudinary.uploader.upload(file.path);
+//       return response.secure_url;
+//     });
+
+//     const images = await Promise.all(uploadedImages);
+
+//     const room = await Room.create({
+//       hotel: hotel._id,
+//       roomType,
+//       pricePerNight: +pricePerNight,
+//       amenities: JSON.parse(amenities),
+//       images
+//     });
+
+//     res.json({
+//       success:true,
+//       message:"Room created successfully",
+//       room
+//     });
+
+//   } catch(error){
+//     console.log("CREATE ROOM ERROR:", error);
+//     res.json({ success:false, message:error.message });
+//   }
+// };
+
+
+// // get all rooms
+// export const getRooms = async (req, res) => {
+//   try {
+
+//     const rooms = await Room.find({ isAvailable:true })
+//       .populate({
+//         path:"hotel",
+//         populate:{
+//           path:"owner",
+//           select:"image"
+//         }
+//       })
+//       .sort({ createdAt:-1 });
+
+//     res.json({ success:true, rooms });
+
+//   } catch(error){
+//     console.log("GET ROOMS ERROR:", error);
+//     res.json({ success:false, message:error.message });
+//   }
+// };
+
+
+// // owner rooms
+// export const getOwnerRooms = async (req, res) => {
+//   try {
+
+//     const userId = req.user._id;
+
+//     const hotelData = await Hotel.findOne({ owner:userId });
+
+//     if(!hotelData){
+//       return res.json({ success:false, message:"Hotel not found" });
+//     }
+
+//     const rooms = await Room.find({
+//       hotel:hotelData._id
+//     }).populate("hotel");
+
+//     res.json({ success:true, rooms });
+
+//   } catch(error){
+//     console.log("OWNER ROOMS ERROR:", error);
+//     res.json({ success:false, message:error.message });
+//   }
+// };
+
+
+// // toggle availability
+// export const toggleRoomAvailability = async (req, res) => {
+//   try {
+
+//     const { roomId } = req.body;
+
+//     const roomData = await Room.findById(roomId);
+
+//     if(!roomData){
+//       return res.json({ success:false, message:"Room not found" });
+//     }
+
+//     roomData.isAvailable = !roomData.isAvailable;
+
+//     await roomData.save();
+
+//     res.json({
+//       success:true,
+//       message:"Room availability toggled successfully",
+//       room:roomData
+//     });
+
+//   } catch(error){
+//     console.log("TOGGLE ROOM ERROR:", error);
+//     res.json({ success:false, message:error.message });
+//   }
+// };
+
+
 import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
-import { v2 as cloudinary } from "cloudinary";
 
-// create room
+// ---------------- CREATE ROOM ----------------
 export const createRoom = async (req, res) => {
   try {
-
     const userId = req.user._id;
 
-    const { roomType, pricePerNight, amenities } = req.body;
+    const { roomType, pricePerNight, amenities, images } = req.body;
 
     const hotel = await Hotel.findOne({ owner: userId });
 
     if (!hotel) {
-      return res.json({ success:false, message:"Hotel not found" });
+      return res.json({ success: false, message: "Hotel not found" });
     }
-
-    const uploadedImages = req.files.map(async (file)=>{
-      const response = await cloudinary.uploader.upload(file.path);
-      return response.secure_url;
-    });
-
-    const images = await Promise.all(uploadedImages);
 
     const room = await Room.create({
       hotel: hotel._id,
       roomType,
-      pricePerNight: +pricePerNight,
-      amenities: JSON.parse(amenities),
-      images
+      pricePerNight: Number(pricePerNight),
+      amenities: amenities || [],
+      images: images || [],
     });
 
     res.json({
-      success:true,
-      message:"Room created successfully",
-      room
+      success: true,
+      message: "Room created successfully",
+      room,
     });
 
-  } catch(error){
+  } catch (error) {
     console.log("CREATE ROOM ERROR:", error);
-    res.json({ success:false, message:error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
 
-// get all rooms
+// ---------------- GET ALL ROOMS ----------------
 export const getRooms = async (req, res) => {
   try {
 
-    const rooms = await Room.find({ isAvailable:true })
+    const rooms = await Room.find({ isAvailable: true })
       .populate({
-        path:"hotel",
-        populate:{
-          path:"owner",
-          select:"image"
-        }
+        path: "hotel",
+        populate: {
+          path: "owner",
+          select: "image",
+        },
       })
-      .sort({ createdAt:-1 });
+      .sort({ createdAt: -1 });
 
-    res.json({ success:true, rooms });
+    res.json({ success: true, rooms });
 
-  } catch(error){
+  } catch (error) {
     console.log("GET ROOMS ERROR:", error);
-    res.json({ success:false, message:error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
 
-// owner rooms
+// ---------------- OWNER ROOMS ----------------
 export const getOwnerRooms = async (req, res) => {
   try {
 
     const userId = req.user._id;
 
-    const hotelData = await Hotel.findOne({ owner:userId });
+    const hotelData = await Hotel.findOne({ owner: userId });
 
-    if(!hotelData){
-      return res.json({ success:false, message:"Hotel not found" });
+    if (!hotelData) {
+      return res.json({ success: false, message: "Hotel not found" });
     }
 
     const rooms = await Room.find({
-      hotel:hotelData._id
+      hotel: hotelData._id,
     }).populate("hotel");
 
-    res.json({ success:true, rooms });
+    res.json({ success: true, rooms });
 
-  } catch(error){
+  } catch (error) {
     console.log("OWNER ROOMS ERROR:", error);
-    res.json({ success:false, message:error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
 
-// toggle availability
+// ---------------- TOGGLE ROOM AVAILABILITY ----------------
 export const toggleRoomAvailability = async (req, res) => {
   try {
 
@@ -100,8 +214,8 @@ export const toggleRoomAvailability = async (req, res) => {
 
     const roomData = await Room.findById(roomId);
 
-    if(!roomData){
-      return res.json({ success:false, message:"Room not found" });
+    if (!roomData) {
+      return res.json({ success: false, message: "Room not found" });
     }
 
     roomData.isAvailable = !roomData.isAvailable;
@@ -109,13 +223,13 @@ export const toggleRoomAvailability = async (req, res) => {
     await roomData.save();
 
     res.json({
-      success:true,
-      message:"Room availability toggled successfully",
-      room:roomData
+      success: true,
+      message: "Room availability toggled successfully",
+      room: roomData,
     });
 
-  } catch(error){
+  } catch (error) {
     console.log("TOGGLE ROOM ERROR:", error);
-    res.json({ success:false, message:error.message });
+    res.json({ success: false, message: error.message });
   }
 };
--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -5,7 +5,8 @@ import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from "../
 
 const roomRouter = express.Router();
 
-roomRouter.post("/", upload.array("images", 4), protect,createRoom);
+// roomRouter.post("/", upload.array("images", 4), protect,createRoom);
+router.post("/", protect, createRoom);
 roomRouter.get("/", getRooms);
 roomRouter.get("/owner", protect, getOwnerRooms);
 roomRouter.post("/toggle-availability", protect, toggleRoomAvailability);
