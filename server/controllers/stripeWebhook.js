a8cad053850b2e75938153a08ca3b4a45a91c8d8
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-03-09 00:40:08 +0530

    Updating the stripe webhook logic

--- server/controllers/stripeWebhook.js
+++ server/controllers/stripeWebhook.js
@@ -1,39 +1,38 @@
-import stripe from 'stripe';
-import Booking from '../models/Booking.js';
+import stripe from "stripe";
+import Booking from "../models/Booking.js";
 
-//API to handle stripe webhooks
 export const stripeWebhook = async (req, res) => {
-    const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
-    const sig = req.headers['stripe-signature'];
-    let event;
-    try {
-        event = stripeInstance.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-    } catch (err) {
-         res.status(400).send(`Webhook Error: ${err.message}`);
-    }
+  const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
 
-    // Handle the event
-    if(event.type === 'payment_intent.succeeded') {
-        // Process the successful payment
-        const paymentIntent = event.data.object;
-        const paymentId = paymentIntent.id;
+  const sig = req.headers["stripe-signature"];
+  let event;
 
+  try {
+    event = stripeInstance.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    console.log("❌ WEBHOOK VERIFY ERROR:", err.message);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
 
-        //getting session id from metadata
-        const session = stripeInstance.checkout.sessions.list ({
-            payment_intent: paymentId,
-        });
+  // ✅ CORRECT EVENT
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
 
-        const {bookingId} = session.data[0].metadata;
+    const bookingId = session.metadata.bookingId;
 
-       //mark the booking as paid in the database
-       await Booking.findByIdAndUpdate(bookingId, {
-        isPaid: true,
-        paymentMethod: "Stripe"
-       });
-    }
-    else{
-        console.log(`Unhandled event type ${event.type}`);
-    }
-    res.status(200).json({ received: true });
-}
+    console.log("✅ PAYMENT SUCCESS:", bookingId);
+
+    await Booking.findByIdAndUpdate(bookingId, {
+      isPaid: true,
+      paymentMethod: "Stripe",
+    });
+  } else {
+    console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).json({ received: true });
+};
